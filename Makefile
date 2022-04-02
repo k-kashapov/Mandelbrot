f382f5fec4d15a0873d5a606ea7b0624bd00996c
k-kashapov <vanyasik247@gmail.com>
2022-04-02 18:54:06 +0300

    AVX instructions used

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 all: jelly.exe
 
 cpps = main.cpp
-compilerFlags = -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -I include -Ofast
+compilerFlags = -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -I include -mavx -mavx2
+
+flags = -O1
 
 jelly.exe: 	$(cpps)
-			g++ $(cpps) $(compilerFlags) -o jelly.exe
+			g++ $(cpps) $(compilerFlags) $(flags) -o jelly.exe
+
+clean:
+	rm ./jelly.exe
--- main.cpp
+++ main.cpp
@@ -1,65 +1,72 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <stdio.h>
-#include <emmintrin.h>
+#include <x86intrin.h>
 
-const int Scr_w = 800;
-const int Scr_h = 600;
-
-// How much steps to take before painting pixel black
-const int Steps = 255;
+static const int Scr_w = 800;
+static const int Scr_h = 600;
 
 // "Infinity" distance
-const float MaxDistSqr = 100;
+static const __m256 MaxDistSqr = _mm256_set1_ps (100.0);
 
-int DoSteps (float init_x, float init_y)
-{
-	float curr_x = init_x;
-	float curr_y = init_y;
+static const __m256 DX = _mm256_set_ps (7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0);
 
-	int step = 0;
-
-	for (; step < Steps; step++)
-	{
-		float x_sqr  = curr_x * curr_x;
-		float y_sqr  = curr_y * curr_y;
-		float new_xy = curr_x * curr_y;
-
-		float new_x = x_sqr - y_sqr + init_x;
-		float new_y = 2 * new_xy + init_y;
-
-		float dist = x_sqr + y_sqr;
-
-		if (dist > MaxDistSqr) break;
-
-		curr_x = new_x;
-		curr_y = new_y;
-	}
-
-	return step;
-}
-
-int MandelbrotToCanvas (float scale, Vector2 init_pos)
+inline int MandelbrotToCanvas (float scale, Vector2 init_pos, int Steps)
 {
 	for (float y = 0; y < Scr_h; y++)
 	{
 		float init_y = (y - Scr_h / 2) * scale - init_pos.y;
+		float init_x = (0 - Scr_w / 2) * scale - init_pos.x;
 		
-		for (float x = 0; x < Scr_w; x++)
+		for (float x = 0; x < Scr_w; x += 8, init_x += 8 * scale)
 		{
-			float init_x = (x - Scr_w / 2) * scale - init_pos.x;
-			float step   = DoSteps (init_x, init_y);
+			__m256 init_set_x = _mm256_add_ps (_mm256_set1_ps (init_x), _mm256_mul_ps (DX, _mm256_set1_ps (scale)));
+			__m256 init_set_y = _mm256_set1_ps (init_y);
 
-			unsigned char colorScale = (unsigned char) (sin (step / 256 * PI) * 255);
+			__m256 curr_x     = init_set_x;
+			__m256 curr_y     = init_set_y;
+			
+			__m256i step_set  = _mm256_set1_epi32 (0);
+		
+			for (int step = 0; step < Steps; step++)
+			{
+				__m256 x_sqr  = _mm256_mul_ps (curr_x,  curr_x);
+				__m256 y_sqr  = _mm256_mul_ps (curr_y,  curr_y);
+				__m256 new_xy = _mm256_mul_ps (curr_x,  curr_y);
+		
+				__m256 new_x = _mm256_add_ps (_mm256_sub_ps (x_sqr, y_sqr), init_set_x);
+				__m256 new_y = _mm256_add_ps (_mm256_add_ps (new_xy, new_xy), init_set_y);
+		
+				__m256 dist = _mm256_add_ps (x_sqr, y_sqr);
+		
+				__m256 compared = _mm256_cmp_ps (dist, MaxDistSqr, _CMP_LE_OQ);
 
-			DrawPixel ((int) x, (int) y, Color { 0, colorScale, colorScale, 255 });
+				step_set = _mm256_sub_epi32 (step_set, _mm256_castps_si256 (compared));
+
+				int mask = _mm256_movemask_ps (compared);
+
+				if (!mask) break;
+
+				curr_x = new_x;
+				curr_y = new_y;
+			}
+
+			int* int_arr = (int *) &step_set;
+
+			for (int pixel = 0; pixel < 8; pixel++)
+			{	
+				int color = int_arr[pixel];
+				unsigned char colorScale = (unsigned char) (sin (PI * int_arr[pixel] / Steps) * 255);
+
+				DrawPixel ((int) x + pixel, (int) y, Color { 0, colorScale, colorScale, 255 });
+			}
 		}
 	}
 
 	return 0;
 }
 
-int ProcessKeyboard (Vector2 *init_pos, float *scale)
+int ProcessKeyboard (Vector2 *init_pos, float *scale, int *steps)
 {
 	if (IsKeyDown (KEY_Z)) *scale *= 0.9;
 	if (IsKeyDown (KEY_X)) *scale /= 0.9;
@@ -70,8 +77,10 @@ int ProcessKeyboard (Vector2 *init_pos, float *scale)
 	if (IsKeyDown (KEY_W)) init_pos->y += 10 * *scale;
 	if (IsKeyDown (KEY_S)) init_pos->y -= 10 * *scale;
 
+	if (IsKeyDown (KEY_O)) *steps += 4;
+	if (IsKeyDown (KEY_P)) *steps -= 4;
 
-	return 0;
+	return GetKeyPressed();
 }
 
 int Drawing()
@@ -79,14 +88,18 @@ int Drawing()
 	float   scale = 0.01;
 	Vector2 init_pos = { 0, 0 };
 
+	// How much steps to take before painting pixel black
+	int Steps = 256;
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
+
+		ProcessKeyboard (&init_pos, &scale, &Steps);
+		
 		ClearBackground (BLACK);
-
-		ProcessKeyboard (&init_pos, &scale);
-
-		MandelbrotToCanvas (scale, init_pos);
+			
+		MandelbrotToCanvas (scale, init_pos, Steps);
 
 		EndDrawing();		
 	}
