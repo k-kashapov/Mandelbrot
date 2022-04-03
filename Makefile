0dd178342c4455033d21822ae15b4fffc8ab7fbb
k-kashapov <vanyasik247@gmail.com>
2022-04-03 23:50:39 +0300

    asm code changed

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all: jelly.exe
 cpps = main.cpp
 compilerFlags = -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -I include -mavx -mavx2
 
-opt_flags = -O2
+opt_flags = -O1
 
 jelly.exe: 	$(cpps)
 			g++ $(cpps) $(compilerFlags) $(opt_flags) -o jelly.exe
--- jelly.s
+++ jelly.s
@@ -1,6 +1,5 @@
 	.file	"main.cpp"
 	.text
-	.p2align 4
 	.globl	_Z15ProcessKeyboardP7Vector2PfPi
 	.type	_Z15ProcessKeyboardP7Vector2PfPi, @function
 _Z15ProcessKeyboardP7Vector2PfPi:
@@ -10,15 +9,15 @@ _Z15ProcessKeyboardP7Vector2PfPi:
 	pushq	%r12
 	.cfi_def_cfa_offset 16
 	.cfi_offset 12, -16
-	movq	%rdx, %r12
 	pushq	%rbp
 	.cfi_def_cfa_offset 24
 	.cfi_offset 6, -24
-	movq	%rsi, %rbp
 	pushq	%rbx
 	.cfi_def_cfa_offset 32
 	.cfi_offset 3, -32
 	movq	%rdi, %rbx
+	movq	%rsi, %rbp
+	movq	%rdx, %r12
 	movl	$90, %edi
 	call	IsKeyDown@PLT
 	testb	%al, %al
@@ -89,21 +88,21 @@ _Z15ProcessKeyboardP7Vector2PfPi:
 	je	.L9
 	subl	$4, (%r12)
 .L9:
+	call	GetKeyPressed@PLT
 	popq	%rbx
 	.cfi_def_cfa_offset 24
 	popq	%rbp
 	.cfi_def_cfa_offset 16
 	popq	%r12
 	.cfi_def_cfa_offset 8
-	jmp	GetKeyPressed@PLT
+	ret
 	.cfi_endproc
 .LFE5895:
 	.size	_Z15ProcessKeyboardP7Vector2PfPi, .-_Z15ProcessKeyboardP7Vector2PfPi
 	.section	.rodata.str1.1,"aMS",@progbits,1
-.LC8:
+.LC10:
 	.string	"FPS = %d\n"
 	.text
-	.p2align 4
 	.globl	_Z7Drawingv
 	.type	_Z7Drawingv, @function
 _Z7Drawingv:
@@ -128,20 +127,122 @@ _Z7Drawingv:
 	.cfi_escape 0x10,0xd,0x2,0x76,0x68
 	.cfi_escape 0x10,0xc,0x2,0x76,0x60
 	pushq	%rbx
-	addq	$-128, %rsp
+	subq	$192, %rsp
 	.cfi_escape 0x10,0x3,0x2,0x76,0x50
 	movq	%fs:40, %rax
 	movq	%rax, -56(%rbp)
 	xorl	%eax, %eax
 	movl	$0x3c23d70a, -128(%rbp)
-	movq	$0, -120(%rbp)
+	movl	$0x00000000, -120(%rbp)
+	movl	$0x00000000, -116(%rbp)
 	movl	$256, -124(%rbp)
+	jmp	.L20
+.L28:
+	vmovdqa	%ymm4, -112(%rbp)
+.L16:
+	movl	$0, %r12d
+	leaq	-112(%rbp), %r15
+	vxorps	%xmm6, %xmm6, %xmm6
+	vcvtsi2ssl	%r14d, %xmm6, %xmm0
+	vmovss	%xmm0, -180(%rbp)
+.L14:
+	vxorps	%xmm3, %xmm3, %xmm3
+	vcvtsi2ssl	(%r15,%r12,4), %xmm3, %xmm0
+	vmulss	.LC6(%rip), %xmm0, %xmm0
+	vdivss	-180(%rbp), %xmm0, %xmm0
+	call	sinf@PLT
+	vmulss	.LC7(%rip), %xmm0, %xmm0
+	vcvttss2sil	%xmm0, %eax
+	movb	$0, %bl
+	movb	%al, %bh
+	movzbl	%al, %eax
+	sall	$16, %eax
+	andl	$-16711681, %ebx
+	orl	%ebx, %eax
+	orl	$-16777216, %eax
+	movl	%eax, %edx
+	movl	%eax, %ebx
+	vcvttss2sil	-132(%rbp), %esi
+	vcvttss2sil	-136(%rbp), %edi
+	addl	%r12d, %edi
+	call	DrawPixel@PLT
+	addq	$1, %r12
+	cmpq	$8, %r12
+	jne	.L14
+	vmovss	-136(%rbp), %xmm2
+	vaddss	.LC8(%rip), %xmm2, %xmm1
+	vmovss	%xmm1, -136(%rbp)
+	vmovss	-144(%rbp), %xmm4
+	vmulss	.LC8(%rip), %xmm4, %xmm0
+	vaddss	-140(%rbp), %xmm0, %xmm6
+	vmovss	%xmm6, -140(%rbp)
+	subl	$1, %r13d
+	je	.L27
+.L18:
+	vbroadcastss	-144(%rbp), %ymm3
+	vmulps	_ZL2DX(%rip), %ymm3, %ymm3
+	vbroadcastss	-140(%rbp), %ymm0
+	vaddps	%ymm0, %ymm3, %ymm3
+	vpxor	%xmm0, %xmm0, %xmm0
+	vmovdqa	%ymm0, -112(%rbp)
+	testl	%r14d, %r14d
+	jle	.L16
+	vmovaps	_ZL10MaxDistSqr(%rip), %ymm6
+	vmovaps	-240(%rbp), %ymm0
+	vmovaps	%ymm3, %ymm2
+	vpxor	%xmm4, %xmm4, %xmm4
+	movl	$0, %edx
+.L17:
+	vmulps	%ymm2, %ymm2, %ymm1
+	vmulps	%ymm0, %ymm0, %ymm5
+	vmulps	%ymm0, %ymm2, %ymm0
+	vsubps	%ymm5, %ymm1, %ymm2
+	vaddps	%ymm3, %ymm2, %ymm2
+	vaddps	%ymm0, %ymm0, %ymm0
+	vaddps	-176(%rbp), %ymm0, %ymm0
+	vaddps	%ymm5, %ymm1, %ymm1
+	vcmpps	$18, %ymm6, %ymm1, %ymm1
+	vpsubd	%ymm1, %ymm4, %ymm4
+	vmovmskps	%ymm1, %eax
+	testl	%eax, %eax
+	je	.L28
+	addl	$1, %edx
+	cmpl	%edx, %r14d
+	jne	.L17
+	vmovdqa	%ymm4, -112(%rbp)
+	jmp	.L16
+.L27:
+	vmovss	-132(%rbp), %xmm5
+	vaddss	.LC9(%rip), %xmm5, %xmm7
+	vmovss	%xmm7, -132(%rbp)
+	subl	$1, -192(%rbp)
+	je	.L29
+.L19:
+	vmovss	-132(%rbp), %xmm6
+	vsubss	.LC5(%rip), %xmm6, %xmm0
+	vmulss	-144(%rbp), %xmm0, %xmm0
+	vsubss	-184(%rbp), %xmm0, %xmm0
+	vmovss	-188(%rbp), %xmm1
+	vmovss	%xmm1, -140(%rbp)
+	movl	$100, %r13d
+	vxorps	%xmm2, %xmm2, %xmm2
+	vmovss	%xmm2, -136(%rbp)
+	vbroadcastss	%xmm0, %ymm7
+	vmovaps	%ymm7, -176(%rbp)
+	vmovaps	%ymm7, -240(%rbp)
+	jmp	.L18
+.L29:
+	call	GetFPS@PLT
+	movl	%eax, %edx
+	leaq	.LC10(%rip), %rsi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	__printf_chk@PLT
+	call	EndDrawing@PLT
+.L20:
 	call	WindowShouldClose@PLT
 	testb	%al, %al
-	jne	.L37
-	.p2align 4,,10
-	.p2align 3
-.L47:
+	jne	.L12
 	call	BeginDrawing@PLT
 	leaq	-124(%rbp), %rdx
 	leaq	-128(%rbp), %rsi
@@ -149,78 +250,24 @@ _Z7Drawingv:
 	call	_Z15ProcessKeyboardP7Vector2PfPi
 	movl	$-16777216, %edi
 	call	ClearBackground@PLT
-	vxorps	%xmm5, %xmm5, %xmm5
-	leaq	-112(%rbp), %rax
-	movl	$600, %ecx
-	vcvtsi2ssl	-124(%rbp), %xmm5, %xmm0
-	movq	%rax, -152(%rbp)
-	leaq	-80(%rbp), %rax
-	vxorps	%xmm2, %xmm2, %xmm2
-	movq	%rax, -160(%rbp)
-	vmovss	%xmm0, -144(%rbp)
-	.p2align 4,,10
-	.p2align 3
-.L40:
-	vcvttss2sil	%xmm2, %ebx
-	movl	$100, %r13d
-	vxorps	%xmm1, %xmm1, %xmm1
-.L39:
-	vcvttss2sil	%xmm1, %r12d
-	movq	-152(%rbp), %r15
-.L38:
-	vxorps	%xmm3, %xmm3, %xmm3
-	movl	%ecx, -140(%rbp)
-	xorb	%r14b, %r14b
-	addq	$4, %r15
-	vcvtsi2ssl	-4(%r15), %xmm3, %xmm0
-	vmulss	.LC4(%rip), %xmm0, %xmm0
-	vmovss	%xmm2, -136(%rbp)
-	vmovss	%xmm1, -132(%rbp)
-	vdivss	-144(%rbp), %xmm0, %xmm0
-	call	sinf@PLT
-	movl	%r14d, %ecx
-	movl	%r12d, %edi
-	movl	%ebx, %esi
-	vmulss	.LC5(%rip), %xmm0, %xmm0
-	addl	$1, %r12d
-	vcvttss2sil	%xmm0, %eax
-	movb	%al, %ch
-	movzbl	%al, %eax
-	movl	%ecx, %r14d
-	sall	$16, %eax
-	andl	$-16711681, %r14d
-	orl	%r14d, %eax
-	orl	$-16777216, %eax
-	movl	%eax, %edx
-	movl	%eax, %r14d
-	call	DrawPixel@PLT
-	cmpq	%r15, -160(%rbp)
-	vmovss	-132(%rbp), %xmm1
-	vmovss	-136(%rbp), %xmm2
-	movl	-140(%rbp), %ecx
-	jne	.L38
-	vaddss	.LC6(%rip), %xmm1, %xmm1
-	subl	$1, %r13d
-	jne	.L39
-	vaddss	.LC7(%rip), %xmm2, %xmm2
-	subl	$1, %ecx
-	jne	.L40
-	call	GetFPS@PLT
-	leaq	.LC8(%rip), %rsi
-	movl	$1, %edi
-	movl	%eax, %edx
-	xorl	%eax, %eax
-	call	__printf_chk@PLT
-	call	EndDrawing@PLT
-	call	WindowShouldClose@PLT
-	testb	%al, %al
-	je	.L47
-.L37:
+	movl	-124(%rbp), %r14d
+	vmovss	-128(%rbp), %xmm5
+	vmovss	%xmm5, -144(%rbp)
+	vmovss	-116(%rbp), %xmm1
+	vmovss	%xmm1, -184(%rbp)
+	vmulss	.LC4(%rip), %xmm5, %xmm0
+	vsubss	-120(%rbp), %xmm0, %xmm2
+	vmovss	%xmm2, -188(%rbp)
+	movl	$600, -192(%rbp)
+	vxorps	%xmm4, %xmm4, %xmm4
+	vmovss	%xmm4, -132(%rbp)
+	jmp	.L19
+.L12:
 	movq	-56(%rbp), %rax
 	xorq	%fs:40, %rax
-	jne	.L48
-	subq	$-128, %rsp
-	xorl	%eax, %eax
+	jne	.L30
+	movl	$0, %eax
+	addq	$192, %rsp
 	popq	%rbx
 	popq	%r10
 	.cfi_remember_state
@@ -233,21 +280,20 @@ _Z7Drawingv:
 	leaq	-8(%r10), %rsp
 	.cfi_def_cfa 7, 8
 	ret
-.L48:
+.L30:
 	.cfi_restore_state
 	call	__stack_chk_fail@PLT
 	.cfi_endproc
 .LFE5896:
 	.size	_Z7Drawingv, .-_Z7Drawingv
 	.section	.rodata.str1.1
-.LC9:
+.LC11:
 	.string	"Mandelbrot"
 	.section	.rodata.str1.8,"aMS",@progbits,1
 	.align 8
-.LC10:
+.LC12:
 	.string	"An error occured during game loop! Terminating..."
-	.section	.text.startup,"ax",@progbits
-	.p2align 4
+	.text
 	.globl	main
 	.type	main, @function
 main:
@@ -257,9 +303,9 @@ main:
 	pushq	%rbx
 	.cfi_def_cfa_offset 16
 	.cfi_offset 3, -16
+	leaq	.LC11(%rip), %rdx
 	movl	$600, %esi
 	movl	$800, %edi
-	leaq	.LC9(%rip), %rdx
 	call	InitWindow@PLT
 	movl	$60, %edi
 	call	SetTargetFPS@PLT
@@ -267,22 +313,21 @@ main:
 	movl	%eax, %ebx
 	call	CloseWindow@PLT
 	testl	%ebx, %ebx
-	jne	.L55
-.L50:
-	xorl	%eax, %eax
+	jne	.L34
+.L32:
+	movl	$0, %eax
 	popq	%rbx
 	.cfi_remember_state
 	.cfi_def_cfa_offset 8
 	ret
-.L55:
+.L34:
 	.cfi_restore_state
-	leaq	.LC10(%rip), %rdi
+	leaq	.LC12(%rip), %rdi
 	call	puts@PLT
-	jmp	.L50
+	jmp	.L32
 	.cfi_endproc
 .LFE5897:
 	.size	main, .-main
-	.p2align 4
 	.type	_GLOBAL__sub_I__Z15ProcessKeyboardP7Vector2PfPi, @function
 _GLOBAL__sub_I__Z15ProcessKeyboardP7Vector2PfPi:
 .LFB5899:
@@ -291,13 +336,12 @@ _GLOBAL__sub_I__Z15ProcessKeyboardP7Vector2PfPi:
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	vmovaps	.LC11(%rip), %ymm0
-	vmovaps	%ymm0, _ZL10MaxDistSqr(%rip)
-	vmovaps	.LC12(%rip), %ymm0
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
+	vmovaps	.LC13(%rip), %ymm0
+	vmovaps	%ymm0, _ZL10MaxDistSqr(%rip)
+	vmovaps	.LC14(%rip), %ymm0
 	vmovaps	%ymm0, _ZL2DX(%rip)
-	vzeroupper
 	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
@@ -322,19 +366,25 @@ _GLOBAL__sub_I__Z15ProcessKeyboardP7Vector2PfPi:
 	.long	1092616192
 	.align 4
 .LC4:
-	.long	1078530011
+	.long	3284664320
 	.align 4
 .LC5:
-	.long	1132396544
+	.long	1133903872
 	.align 4
 .LC6:
-	.long	1090519040
+	.long	1078530011
 	.align 4
 .LC7:
+	.long	1132396544
+	.align 4
+.LC8:
+	.long	1090519040
+	.align 4
+.LC9:
 	.long	1065353216
 	.section	.rodata.cst32,"aM",@progbits,32
 	.align 32
-.LC11:
+.LC13:
 	.long	1120403456
 	.long	1120403456
 	.long	1120403456
@@ -344,7 +394,7 @@ _GLOBAL__sub_I__Z15ProcessKeyboardP7Vector2PfPi:
 	.long	1120403456
 	.long	1120403456
 	.align 32
-.LC12:
+.LC14:
 	.long	0
 	.long	1065353216
 	.long	1073741824
